c0eeeef6e0e48cc26f842c5bf793fafe9d8d98d1
Kanat62 <102471510+Kanat62@users.noreply.github.com>
2024-01-23 18:32:19 +0600

    Update AuthContext.jsx

--- src/utils/AuthContext.jsx
+++ src/utils/AuthContext.jsx
@@ -25,7 +25,7 @@ export const AuthProvider = ({ children }) => {
             cookieFallback === null ||
             cookieFallback === undefined
         ) {
-            navigate("/sign-in");
+            navigate("/login");
         }
         setUser(cookieFallback);
         // getUser();
