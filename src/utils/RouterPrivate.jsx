bd7e17291ba260b009d2cc4aa2f7f1955b1714a9
Kanat62 <102471510+Kanat62@users.noreply.github.com>
2024-01-23 20:59:56 +0600

    Update RouterPrivate.jsx

--- src/utils/RouterPrivate.jsx
+++ src/utils/RouterPrivate.jsx
@@ -1,16 +1,8 @@
 import { Navigate, Outlet } from 'react-router-dom'
 import { useAuth } from "./AuthContext";
-import { useEffect } from "react";
 
 const RouterPrivate = () => {
-    const {user,getUser} = useAuth()
-
-    // useEffect(() => {
-    //     console.log(user);
-    //     getUser()
-    // }, []);
-        console.log(user ? 'user' : 'note found');
-
+    const {user} = useAuth()
     return user ? <Outlet/> : <Navigate to='/login'/>
 }
 
