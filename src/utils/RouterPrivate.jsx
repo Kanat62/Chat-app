5adc381dd431d9eadcbf36aa4b4ca312c5002e84
Kanat62 <102471510+Kanat62@users.noreply.github.com>
2024-01-23 18:39:12 +0600

    Update RouterPrivate.jsx

--- src/utils/RouterPrivate.jsx
+++ src/utils/RouterPrivate.jsx
@@ -10,7 +10,7 @@ const RouterPrivate = () => {
     //     getUser()
     // }, []);
     
-    return user ? <Outlet/> : <Navigate to='/login'/>
+    return user ? <Outlet/> : <Navigate to='/'/>
 }
 
 export default RouterPrivate
